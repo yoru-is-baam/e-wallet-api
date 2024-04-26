87147f15b0ea524210ecd3df41ebda44ef8683d6
yoru-is-baam <hoanghuynhtuankiet69@gmail.com>
2024-04-26 22:44:03 +0700

    docs: add swagger document

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import compression from "compression";
 import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
+
+// Swagger
+import YAML from "yamljs";
+import swaggerUI from "swagger-ui-express";
 
 import routeV1 from "./routes/v1/index.js";
 import logger from "./loggers/winston.log.js";
@@ -20,6 +25,7 @@ import notFoundMiddleware from "./middlewares/not-found.js";
 import errorHandlerMiddleware from "./middlewares/error-handler.js";
 
 const app = express();
+const swaggerDocument = YAML.load("./swagger.yml");
 
 app.use(express.json());
 app.use(
@@ -27,11 +33,12 @@ app.use(
 		extended: true,
 	}),
 );
-app.use(rateLimit(rateLimitOptions));
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 app.use(compression());
+app.use(mongoSanitize());
 app.use(morgan("dev"));
+app.use(rateLimit(rateLimitOptions));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use((req, res, next) => {
@@ -52,6 +59,7 @@ import "./db/init.mongodb.js";
 // import { checkOverload } from "./helpers/check.connect.js";
 // checkOverload();
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use("/api/v1", routeV1);
 
 app.use(notFoundMiddleware);
